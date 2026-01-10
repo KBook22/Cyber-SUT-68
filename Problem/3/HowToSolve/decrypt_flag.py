cfaf06a1e4f89495063143438f2d4eec1586bfc4
JURIBER <chathatpolsermsri@gmail.com>
2026-01-10 17:45:57 +0700

    problem of ju

---
+++ Problem/3/HowToSolve/decrypt_flag.py
@@ -0,0 +1,23 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+import hashlib
+
+# Read the encrypted file
+with open("flag.png.enc", "rb") as f:
+    iv = f.read(16)
+    ciphertext = f.read()
+
+# Generate the key from the passphrase
+key = hashlib.sha256(b"lnwzaCyber").digest()
+
+# Decrypt the image
+cipher = AES.new(key, AES.MODE_CBC, iv)
+plaintext = unpad(cipher.decrypt(ciphertext), 16)
+
+# Save the decrypted image
+with open("flag.png", "wb") as f:
+    f.write(plaintext)
+
+print("✓ Decryption successful!")
+print("✓ flag.png has been extracted")
+print("Then Try exiftool on flag.png to get the flag")
