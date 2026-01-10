2522ba3525723b18da61eee93bacb96103e2ba8e
kittisak <kittisakbook.22@gmail.com>
2026-01-10 18:42:32 +0700

    page admin & doorelden

---
+++ next-app/app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-6xl font-bold text-white">Admin</h1>
+    </div>
+  );
+}
